04f723530a85c3ea4a9adc331eb8aaea514ad135
João Paulo Faria <jpfaria@gmail.com>
2022-12-27 14:45:35 -0300

    wip: impl

--- middleware/contrib/prometheus/client_golang.v1/middleware.go
+++ middleware/contrib/prometheus/client_golang.v1/middleware.go
@@ -2,29 +2,19 @@ package prometheus
 
 import (
 	"github.com/americanas-go/cache"
-	"github.com/americanas-go/log"
 )
 
 type middleware[R any] struct{}
 
 func (l middleware[R]) Del(c *cache.Context[R], s string) error {
-	middleware := log.FromContext(c.GetContext())
-	middleware.Tracef("executing Del method")
-	defer middleware.Debugf("executed Del method")
 	return c.Del(s)
 }
 
 func (l middleware[R]) Get(c *cache.Context[R], s string) ([]byte, error) {
-	middleware := log.FromContext(c.GetContext())
-	middleware.Tracef("executing Get method")
-	defer middleware.Debugf("executed Get method")
 	return c.Get(s)
 }
 
 func (l middleware[R]) Set(c *cache.Context[R], s string, data []byte) error {
-	middleware := log.FromContext(c.GetContext())
-	middleware.Tracef("executing Set method")
-	defer middleware.Debugf("executed Set method")
 	return c.Set(s, data)
 }
 
